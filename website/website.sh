9a3f8a8c3bc62c1acebb0bbb17f5f9647aac48b1
sunflower-knight <dglz.hyj@qq.com>
2023-04-15 06:12:36 +0800

    config generate.sh

--- website/website.sh
+++ website/website.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 set -e
 
-function generate() {
-    ROOT_DIR=$(pwd)
-    cd "$ROOT_DIR" || exit 1
+ROOT_DIR=$(pwd)
+cd "$ROOT_DIR" || exit 1
 
-    # delete website's static dir generated before
-    rm -r dist
-    mkdir dist
+# download doxygen-css warning
+if [ ! -d "doxygen-awesome-css" ]; then
+  echo "warning: doxygen-awesome-css does not exist here."
+fi
 
-    # Commands without errors : copy images from english to chinese dir
-    set +e # Disable -e option temporarily
-    cp -r docs/coro_rpc/images docs/zh/coro_rpc
-    cp -r docs/guide/images docs/zh/guide
-    cp -r docs/struct_pack/images docs/zh/struct_pack
-    cp -r docs/struct_pb/images docs/zh/struct_pb
-    set -e # Re-enable -e option
+# delete website's static dir generated before
+rm -r dist
+mkdir dist
 
-    # generate
-    yarn docs:build
-    doxygen Doxyfile
-    doxygen Doxyfile_cn
-    echo 'Generate Done!'
-}
+# Commands without errors : copy images from english to chinese dir
+set +e # Disable -e option temporarily
+cp -r docs/coro_rpc/images docs/zh/coro_rpc
+cp -r docs/guide/images docs/zh/guide
+cp -r docs/struct_pack/images docs/zh/struct_pack
+cp -r docs/struct_pb/images docs/zh/struct_pb
+set -e # Re-enable -e option
 
-
+# generate
+yarn docs:build
+doxygen Doxyfile
+doxygen Doxyfile_cn
+echo 'Generate Done!'
