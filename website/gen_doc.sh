11cbdd3b8cbb5918f8fb489c39e098a39a717325
sunflower-knight <dglz.hyj@qq.com>
2023-04-15 06:02:46 +0800

    modify generate.sh

--- website/gen_doc.sh
+++ website/gen_doc.sh
@@ -1,9 +1,27 @@
+#!/bin/bash
 set -e
 
-ROOT_DIR=$(pwd)
-cd "$ROOT_DIR" || exit 1
+function generate() {
+    ROOT_DIR=$(pwd)
+    cd "$ROOT_DIR" || exit 1
 
-yarn docs:build
-doxygen Doxyfile
-doxygen Doxyfile_cn
-echo 'Generate Done!'
+    # delete website's static dir generated before
+    rm -r dist
+    mkdir dist
+
+    # Commands without errors : copy images from english to chinese dir
+    set +e # Disable -e option temporarily
+    cp -r docs/coro_rpc/images docs/zh/coro_rpc
+    cp -r docs/guide/images docs/zh/guide
+    cp -r docs/struct_pack/images docs/zh/struct_pack
+    cp -r docs/struct_pb/images docs/zh/struct_pb
+    set -e # Re-enable -e option
+
+    # generate
+    yarn docs:build
+    doxygen Doxyfile
+    doxygen Doxyfile_cn
+    echo 'Generate Done!'
+}
+
+
